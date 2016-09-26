3243b848ca537b6406f5466e9e7d767e49e37caa
lakhansamani <lakhan.m.samani@gmail.com>
2016-09-26 18:47:27 +0530

    Removes unsed actions

--- src/constants/actionType.js
+++ src/constants/actionType.js
@@ -1,5 +1,3 @@
-export const SET_LIST = 'SET_LIST';
-export const GET_LIST = 'GET_LIST';
 export const FETCH_DATA = 'FETCH_DATA';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_ERROR = 'FETCH_DATA_ERROR';
