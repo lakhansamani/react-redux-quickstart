0bc9361c6567f33798dc514c7b32a785d18373bb
lakhansamani <lakhan.m.samani@gmail.com>
2016-10-11 00:42:31 +0530

    Changes fetch library and adds unit test for fetch data action

--- src/actions/ListApi.js
+++ src/actions/ListApi.js
@@ -1,6 +1,7 @@
 import * as api from '../constants/ApiConstants';
+import fetch from 'isomorphic-fetch';
 const getList = (offset) => {
     return fetch(api.getList+"?offset="+offset,{method:'get'})
-    .then((res)=>res.json());
+    .then((res)=>res.json(),(err)=>err);
 };
 export default getList;
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -37,10 +37,5 @@ module.exports = {
     contentBase: './dist',
     hot:true,
     historyApiFallback: true
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
-  ]
+  }
 };
