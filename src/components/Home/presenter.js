8951a482e0a5b6b3458c55fd2652c27827a141f1
lakhansamani <lakhan.m.samani@gmail.com>
2016-10-27 23:59:37 +0530

    Changes new fetch logic

--- src/components/Home/presenter.js
+++ src/components/Home/presenter.js
@@ -6,6 +6,7 @@ class Home extends Component{
   constructor(props){
     super(props);
     this.onScroll = this.onScroll.bind(this);
+		this.state = {scrolling:false}
   }
   componentDidMount(){
     if(this.needToFetch()){
@@ -20,7 +21,8 @@ class Home extends Component{
   onScroll() {
     const {offset,limit} = this.props.list;
     if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500)) {
-      if(this.needToFetch() && this.canFetch()){
+      this.setState({scrolling:true});
+			if(this.needToFetch() && this.canFetch()){
         this.props.getData(offset);
       }
     }
@@ -31,17 +33,19 @@ class Home extends Component{
     if(list.length === 0){
       return true;
     }
+		if(this.state.scrolling){
+			if(total === list.length){
+      	return false;
+    	}
+    	if(list.length<12){
+      	return false;
+    	}
+    	if(offset > total){
+      	return false;
+    	}
+    	return true;
 
-    if(total === list.length){
-      return false;
-    }
-    if(list.length<12){
-      return false;
-    }
-    if(offset > total){
-      return false;
-    }
-    return true;
+		}
   }
   canFetch(){
     const {loading} = this.props.list;
